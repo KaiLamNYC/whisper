29fdb286f7b8bb4504732c4522173ec22d538eee
Kai Lam <kailamnyc@gmail.com>
2023-10-04 01:31:07 -0400

    finished the chat routes and functions

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -49,6 +49,131 @@ async function accessChat(req, res, next) {
 	}
 }
 
+async function fetchChats(req, res, next) {
+	try {
+		const result = await Chat.find({
+			users: { $elemMatch: { $eq: req.userId } },
+		})
+			.populate("users", "-password")
+			.populate("groupAdmin", "-password")
+			.populate("latestMessage")
+			.sort({ updatedAt: -1 });
+		// .then(async (results) => {
+		// 	results = await User.populate(results, {
+		// 		path: "latestMessage.sender",
+		// 		select: "username",
+		// 	});
+		// });
+
+		res.json({
+			message: "success getchats",
+			payload: result,
+		});
+	} catch (e) {
+		res.json({
+			message: "failure",
+			payload: `failed to getAllChats ${e}`,
+		});
+	}
+}
+
+async function createGroupChat(req, res, next) {
+	try {
+		if (!req.body.name) {
+			return res.status(400).send({ message: "Please Fill all the feilds" });
+		}
+
+		var users = JSON.parse(req.body.users);
+
+		if (users.length < 2) {
+			return res
+				.status(400)
+				.send("More than 2 users are required to form a group chat");
+		}
+
+		users.push(req.userId);
+		const groupChat = await Chat.create({
+			chatName: req.body.name,
+			users: users,
+			isGroupChat: true,
+			groupAdmin: req.userId,
+		});
+
+		const fullGroupChat = await Chat.findOne({ _id: groupChat._id }).populate(
+			"users",
+			"-password"
+		);
+
+		res.json({
+			payload: fullGroupChat,
+		});
+	} catch (e) {
+		res.json({
+			message: "failure",
+			payload: `failed to creategroupchat ${e}`,
+		});
+	}
+}
+
+async function addToGroupChat(req, res, next) {
+	try {
+		const { chatId, userId } = req.body;
+
+		//FINDING CHAT AND PUSHING NEW USER TO GROUP
+		const added = await Chat.findByIdAndUpdate(
+			chatId,
+			{ $push: { users: userId } },
+			{ new: true }
+		)
+			.populate("users", "-password")
+			.populate("groupAdmin", "-password");
+
+		if (!added) {
+			res.status(404);
+			throw new Error("Chat Not Found");
+		} else {
+			res.json({ payload: added });
+		}
+	} catch (e) {
+		res.json({
+			message: "failure",
+			payload: `failed to addtogroup ${e}`,
+		});
+	}
+}
+
+async function removeFromGroupChat(req, res, next) {
+	try {
+		const { chatId, userId } = req.body;
+
+		const removed = await Chat.findByIdAndUpdate(
+			chatId,
+			{ $pull: { users: userId } },
+			{ new: true }
+		)
+			.populate("users", "-password")
+			.populate("groupAdmin", "-password");
+
+		if (!removed) {
+			res.status(404);
+			throw new Error("Chat Not Found");
+		} else {
+			res.json({
+				payload: removed,
+			});
+		}
+	} catch (e) {
+		res.json({
+			message: "failure",
+			payload: `failed to addtogroup ${e}`,
+		});
+	}
+}
+
 module.exports = {
 	accessChat,
+	fetchChats,
+	createGroupChat,
+	addToGroupChat,
+	removeFromGroupChat,
 };
--- server/routes/chatRouter.js
+++ server/routes/chatRouter.js
@@ -2,16 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 //FUNCTIONS
-const { accessChat } = require("../controllers/chatController");
+const {
+	accessChat,
+	fetchChats,
+	createGroupChat,
+	addToGroupChat,
+	removeFromGroupChat,
+} = require("../controllers/chatController");
 
 const verifyJWT = require("../controllers/middleware/verifyJWT");
 
 //ROUTES
 router.post("/accessChat", verifyJWT, accessChat);
-// router.get("/", verifyJWT, fetchChats);
-// router.post("/createGroupChat", verifyJWT, createGroupChat);
-// router.put("/renameGroupChat", verifyJWT, renameGroupChat);
-// router.put("/removeFromGroup", verifyJWT, removeFromGroupChat);
-// router.put("/addToGroup", verifyJWT, addToGroup);
+router.get("/", verifyJWT, fetchChats);
+router.post("/createGroupChat", verifyJWT, createGroupChat);
+router.put("/removeFromGroup", verifyJWT, removeFromGroupChat);
+router.put("/addToGroupChat", verifyJWT, addToGroupChat);
 
 module.exports = router;
